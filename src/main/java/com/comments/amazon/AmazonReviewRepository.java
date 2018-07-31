2bd32c40041eb0ca6ae9f4034f4bdbd97db72235
JaydeeCee <41925784+JaydeeCee@users.noreply.github.com>
2018-07-31 22:59:37 +0100

    Update AmazonReviewRepository.java

--- src/main/java/com/comments/amazon/AmazonReviewRepository.java
+++ src/main/java/com/comments/amazon/AmazonReviewRepository.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
+// Repository for jpa functionalities for amazon review Entities
 @Repository
 public interface AmazonReviewRepository extends JpaRepository<AmazonScrapperEntity, Long>{
 	
